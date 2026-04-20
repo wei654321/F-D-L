86afeba3f3665f99c97e9d83dba8d6fea111c3c9
wei654321 <2664492585@qq.com>
2026-04-21 01:47:37 +0800

    Update fgourl.py

--- fgourl.py
+++ fgourl.py
@@ -1,28 +1,3 @@
-import json
-import binascii
-import requests
-import main
-import CatAndMouseGame
-import os
-
-requests.urllib3.disable_warnings()
-session = requests.Session()
-session.verify = False
-
-# ===== Game's parameters =====
-app_ver_ = ''
-data_ver_ = 0
-date_ver_ = 0
-ver_code_ = ''
-asset_bundle_folder_ = ''
-data_server_folder_crc_ = 0
-server_addr_ = "https://game.fate-go.jp"
-github_token_ = ''
-github_name_ = ''
-
-
-# ==== User Info ====
-def set_latest_assets():
     global app_ver_, data_ver_, date_ver_, asset_bundle_folder_, data_server_folder_crc_, ver_code_, server_addr_
 
     region = main.fate_region
@@ -33,9 +8,7 @@ def set_latest_assets():
         server_addr_ = "https://game.fate-go.us"
 
     # Get Latest Version of the data!
-    version_str = main.get_latest_appver()
-    #main.logger.info(f"vv{version_str}")
-
+    version_str = version.get_version(region)
     response = requests.get(
         server_addr_ + '/gamedata/top?appVer=' + version_str).text
     response_data = json.loads(response)["response"][0]["success"]
@@ -45,8 +18,6 @@ def set_latest_assets():
     data_ver_ = response_data['dataVer']
     date_ver_ = response_data['dateVer']
     ver_code_ = main.get_latest_verCode()
-
-    #main.logger.info(f"ver{ver_code_}")
 
     # Use Asset Bundle Extractor to get Folder Name
     assetbundle = CatAndMouseGame.getAssetBundle(response_data['assetbundle'])
@@ -68,7 +39,7 @@ httpheader = {
     'User-Agent': user_agent_2,
     'Accept-Encoding': "deflate, gzip",
     'Content-Type': "application/x-www-form-urlencoded",
-    'X-Unity-Version': "2022.3.62f2"
+    'X-Unity-Version': "2020.3.34f1"
 
 }
 
